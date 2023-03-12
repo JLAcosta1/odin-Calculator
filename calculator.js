83c368133f762fadf927c1caf639cdec104289e6
JLAcosta1 <mathassistant3.14@gmail.com>
2023-03-11 23:41:14 -0700

    Make buttons print nums and operators to console

---
+++ calculator.js
@@ -0,0 +1,66 @@
+function printOne() {
+    console.log("1");
+}
+function printTwo() {
+    console.log("2");
+}
+function printThree() {
+    console.log("3");
+}
+function printFour() {
+    console.log("4");
+}
+function printFive() {
+    console.log("5");
+}
+function printSix() {
+    console.log("6");
+}
+function printSeven() {
+    console.log("7");
+}
+function printEight() {
+    console.log("8");
+}
+function printNine() {
+    console.log("9");
+}
+function printZero() {
+    console.log("0");
+}
+function printAdd() {
+    console.log("+");
+}
+function printSubtract () {
+    console.log("-");
+}
+function printMultiply() {
+    console.log("+");
+}
+function printDivide() {
+    console.log("/");
+}
+function printEqual() {
+    console.log("=");
+}
+function printDecimal() {
+    console.log(".");
+}
+
+
+one.addEventListener("click", printOne);
+two.addEventListener("click", printTwo);
+three.addEventListener("click", printThree);
+four.addEventListener("click", printFour);
+five.addEventListener("click", printFive);
+six.addEventListener("click", printSix);
+seven.addEventListener("click", printSeven);
+eight.addEventListener("click", printEight);
+nine.addEventListener("click", printNine);
+zero.addEventListener("click", printZero);
+add.addEventListener("click", printAdd);
+subtract.addEventListener("click", printSubtract);
+multiply.addEventListener("click", printMultiply);
+divide.addEventListener("click", printDivide);
+equal.addEventListener("click", printEqual);
+decimal.addEventListener("click", printDecimal);
